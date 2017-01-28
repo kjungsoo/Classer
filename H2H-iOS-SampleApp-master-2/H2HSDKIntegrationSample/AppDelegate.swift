c048c3dbe3cade4124c6a01d8484e295e7dfd89f
Rmurthy1 <rahul.a.murthy@gmail.com>
2017-01-28 10:09:40 -0800

    Appdelegate

--- H2H-iOS-SampleApp-master-2/H2HSDKIntegrationSample/AppDelegate.swift
+++ H2H-iOS-SampleApp-master-2/H2HSDKIntegrationSample/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, NSURLSessionDelegate {
 
     var window: UIWindow?
 
+    var course: ClassInfo?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
