8f638ea570e29688a23e43b6e5f1deda4078a729
ss42 <sanjay.shrestha1988@gmail.com>
2017-01-28 00:24:14 -0800

    view controller added

--- H2H-iOS-SampleApp-master-2/ViewClassesViewController.swift
+++ H2H-iOS-SampleApp-master-2/ViewClassesViewController.swift
@@ -17,12 +17,41 @@ class ViewClassesViewController: UIViewController, UITableViewDelegate, UITableV
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        // Do any additional setup after loading the view.
+        // navigation title & color
+        self.navigationItem.title = "Available Classes"
+        self.navigationController?.navigationBar.tintColor
+            = UIColor(red: 255/255.0, green: 107/255.0, blue: 100/255.0, alpha: 1.0)
+        // Create the navigation bar
+        let navigationBar = UINavigationBar(frame: CGRectMake(0, 0, self.view.frame.size.width, 44)) // Offset by 20 pixels vertically to take the status bar into account
+        
+        navigationBar.backgroundColor = UIColor.whiteColor()
+        //navigationBar.delegate = self
+        
+        // Create a navigation item with a title
+        let navigationItem = UINavigationItem()
+        
+        // Create left and right button for navigation item
+        let rightButton = UIBarButtonItem(title: "Right", style: UIBarButtonItemStyle.Plain, target: self, action: "createClass:")
+        
+        // Create two buttons for the navigation item
+        navigationItem.rightBarButtonItem = rightButton
+        
+        // Assign the navigation item to the navigation bar
+        navigationBar.items = [navigationItem]
+        
+        // Make the navigation bar a subview of the current view controller
+        self.view.addSubview(navigationBar)
+        
+        
+              // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func createClass(sender: UIBarButtonItem) {
+        // Do something
     }
     
 
